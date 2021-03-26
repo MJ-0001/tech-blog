60c49eafda79c59a7ffcd2049a92e7fecdf83d9d
Matthew Jones <mejones92@gmail.com>
2021-03-26 22:27:12 +0000

    Updated schema name.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
-DROP DATABASE IF EXISTS 
-CREATE DATABASE 
-USE
+-- If this db already exists, drop it
+DROP DATABASE IF EXISTS tech_blog_db;
+
+-- Create a new db
+CREATE DATABASE tech_blog_db;
+
+-- Select the db for use in mysql
+USE tech_blog_db;
