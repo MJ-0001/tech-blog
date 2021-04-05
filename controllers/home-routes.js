edf6fa5f0022a0c0f0b7b3aa515cc79c253d70c3
Matthew Jones <mejones92@gmail.com>
2021-04-05 17:26:48 +0100

    Added route for home landing page.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { User, Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get("/", function(req, res) {
-  res.render('main');
+router.get('/', (req, res) => {
+  res.render('home');
 });
 
 router.get('/', async (req, res) => {
